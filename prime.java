d9da9a30213774ff15a46927f6c1c411dd75434a
Vedanshkatara <vedanshkatara161@gmail.com>
2025-06-09 09:22:20 +0530

    Added code to prime.java

--- prime.java
+++ prime.java
@@ -3,7 +3,7 @@ public class PrimeNumber {
     public static void main(String[] args) {
         int num = 29;
         boolean isPrime = true;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= num/2 ; i++) {
             if (num % i == 0) {
                 isPrime = false;
                 break;
