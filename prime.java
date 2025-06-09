719d07435f048af946fbd545ec01434a7b272c01
Vedanshkatara <vedanshkatara161@gmail.com>
2025-06-09 09:18:08 +0530

    Added code to prime.java

---
+++ prime.java
@@ -0,0 +1,14 @@
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(num + " is " + (isPrime ? "a prime number." : "not a prime number."));
+    }
+}
